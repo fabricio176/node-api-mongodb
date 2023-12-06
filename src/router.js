2de956aca34d73367b5e31d70b8dbdd832aab4e5
Fabricio Taveira Carvalho Marques <tr26330es@trf1.gov.br>
2023-12-06 08:48:37 -0300

    Adding the options delete and put to the API

--- src/router.js
+++ src/router.js
@@ -2,13 +2,27 @@ const express = require('express');
 const User = require('./models/User')
 const router = express.Router();
 
+router.put("/:id", async(req, res)=> {
+    const user = await User.findByIdAndUpdate(req.params.id, {
+        id: req.body.id,
+        email: req.body.email,
+        senha: req.body.senha,
+        telefoneDDD: req.body.telefoneDDD,
+        telefoneNumero: req.body.telefoneNumero,
+        dataCriacao: req.body.dataCriacao,
+        dataAtualizacao: req.body.dataAtualizacao,
+        ultimoLogin: req.body.ultimoLogin
+    })
+    return res.send(user);
+})
+
 router.get('/', async (req,res) =>{
     const users = await User.find();
     res.send(users);
 });
 
 router.delete('/:id', async(req, res) =>{
-    const user = await user.findById(req.params.id);
+    const user = await User.findByIdAndRemove(req.params.id);
     res.send(user);
 })
 

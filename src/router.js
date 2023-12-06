63fd47c17968565a24da7cfacfe96a9f8468dc85
Fabricio Taveira Carvalho Marques <tr26330es@trf1.gov.br>
2023-12-06 09:16:49 -0300

    Adding treatment exception

--- src/router.js
+++ src/router.js
@@ -2,44 +2,66 @@ const express = require('express');
 const User = require('./models/User')
 const router = express.Router();
 
-router.put("/:id", async(req, res)=> {
-    const user = await User.findByIdAndUpdate(req.params.id, {
-        id: req.body.id,
-        email: req.body.email,
-        senha: req.body.senha,
-        telefoneDDD: req.body.telefoneDDD,
-        telefoneNumero: req.body.telefoneNumero,
-        dataCriacao: req.body.dataCriacao,
-        dataAtualizacao: req.body.dataAtualizacao,
-        ultimoLogin: req.body.ultimoLogin
-    })
-    return res.send(user);
+router.put("/:id", async (req, res) => {
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, {
+            id: req.body.id,
+            email: req.body.email,
+            senha: req.body.senha,
+            telefoneDDD: req.body.telefoneDDD,
+            telefoneNumero: req.body.telefoneNumero
+        });
+        if (!user) {
+            return res.status(404).send("Usuário não encontrado")
+        }
+        res.send(user);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
 })
 
-router.get('/', async (req,res) =>{
-    const users = await User.find();
-    res.send(users);
+router.get('/', async (req, res) => {
+    try {
+        const users = await User.find();
+        if (!users) {
+            res.status(404).send("Nenhum registrado até o momento");
+        }
+        res.send(users);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
 });
 
-router.delete('/:id', async(req, res) =>{
-    const user = await User.findByIdAndRemove(req.params.id);
-    res.send(user);
+router.delete('/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+        if (!user) {
+            return res.status(404).send("Usuário Não encontrado")
+        }
+        res.send(user);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
 })
 
 router.post("/", async (req, res) => {
-    const user = new User({
-        id: req.body.id,
-        email: req.body.email,
-        senha: req.body.senha,
-        telefoneDDD: req.body.telefoneDDD,
-        telefoneNumero: req.body.telefoneNumero,
-        dataCriacao: req.body.dataCriacao,
-        dataAtualizacao: req.body.dataAtualizacao,
-        ultimoLogin: req.body.ultimoLogin
-    })
+    try {
+        const user = new User({
+            id: req.body.id,
+            email: req.body.email,
+            senha: req.body.senha,
+            telefoneDDD: req.body.telefoneDDD,
+            telefoneNumero: req.body.telefoneNumero,
+            dataCriacao: req.body.dataCriacao,
+            dataAtualizacao: req.body.dataAtualizacao,
+            ultimoLogin: req.body.ultimoLogin
+        })
 
-    await user.save();
-    res.send(user);
+        await user.save();
+        res.send(user);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
 });
 
 module.exports = router;
